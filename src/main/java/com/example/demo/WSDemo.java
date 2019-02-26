1f79b81d47597727481dcc22182d110f750dac75
AnshumanM-hexaware <47624782+AnshumanM-hexaware@users.noreply.github.com>
2019-02-26 15:53:00 +0530

    kar lo bhai Commited

--- src/main/java/com/example/demo/WSDemo.java
+++ src/main/java/com/example/demo/WSDemo.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -14,13 +15,26 @@ import com.example.demo.Practice;
 @Controller
 @CrossOrigin
 @RestController
+@RequestMapping("/MiniJumbo")
 public class WSDemo {
-	@RequestMapping("/preview")
-	public @ResponseBody List<String> getSchemasService() {
+	
+	//service for getting schema names.
+	@RequestMapping("/getSchema")
+	public @ResponseBody List<String> getSchemasService() throws ClassNotFoundException, SQLException {
 		return Practice.getSchema();
 	}
-	@RequestMapping(path = "/preview/{schema}", method = RequestMethod.GET)
+	
+	//service for getting table names.
+	@RequestMapping(path = "/getTables/{schema}", method = RequestMethod.GET)
 	public @ResponseBody List<String> getTablesService(@PathVariable String schema) {
 		return Practice.getTables(schema);
 	}
+	
+	//service for getting column names.
+	@RequestMapping(path = "/getColumns/{schema}/{table_name}", method = RequestMethod.GET)
+	public @ResponseBody List<String> getColumnsService(@PathVariable String schema, @PathVariable String table_name) {
+		return Practice.getColumns(schema,table_name);
+	}
+	
+	
 }
